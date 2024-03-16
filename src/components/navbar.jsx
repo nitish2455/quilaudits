100acb328d0cf1e7d3333c7dc57b97b3427c5764
Aryan Raj <aryanraj.raushan@gmail.com>
2024-03-16 14:47:46 +0530

    Quillshield

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
     
     return (
-      <div className="flex justify-center gap-8 bg-transparent z-50 mt-3">
-        <div className="flex gap-2 pt-2">
+      <div className="flex items-center justify-evenly gap-8 bg-transparent z-50 mt-3">
+        <div className="flex gap-2 pt-2 ml-10">
           <img className="w-[35px] h-[30px]  z-[6]" alt="" src="/icon.svg" />
           <img
             className="w-[120px] h-[30px]  z-[6] "
@@ -12,17 +12,21 @@ const Navbar = () => {
             src="/quillaudits.svg"
           />
         </div>
-        <button className="text-white text-sm">Services</button>
-        <button className="text-white text-sm">Pricing</button>
-        <button className="text-white text-sm">Our Audits</button>
-        <button className="text-white text-sm">Tools</button>
-        <button className="text-white text-sm">Resources</button>
-        <button className="text-white text-sm">Refer-earn-secure</button>
-        <button className="cursor-pointer [border:none] p-2  bg-[transparent]  rounded-[8.03px] [background:linear-gradient(266.04deg,_#7184fd_20%,_#3f7af0_50%,_#4f37ee)] shadow-[0px_13.4px_20.08px_-4.02px_rgba(0,_0,_0,_0.1),_0px_5.4px_8.03px_-5.36px_rgba(0,_0,_0,_0.1)] overflow-hidden shrink-0 flex flex-row items-start justify-start box-border whitespace-nowrap z-[6]">
-          <div className="flex-1 relative text-2xl-4 leading-[32.13px] font-medium font-jost text-white text-center">
-            Request An Audit
-          </div>
-        </button>
+        <div className="flex items-center gap-8 text-base text-white">
+          <button className="">Services</button>
+          <button className="">Pricing</button>
+          <button className="">Our Audits</button>
+          <button className="">Tools</button>
+          <button className="">Resources</button>
+          <button className="">Refer-earn-secure</button>
+        </div>
+        <div className="lg:pr-24">
+          <button className="cursor-pointer border-none  bg-[transparent] [background:linear-gradient(266.04deg,_#7184fd_20%,_#3f7af0_50%,_#4f37ee)] shadow-[0px_13.4px_20.08px_-4.02px_rgba(0,_0,_0,_0.1),_0px_5.4px_8.03px_-5.36px_rgba(0,_0,_0,_0.1)] shrink-0 box-border whitespace-nowrap z-[6] rounded-md">
+            <div className="relative text-base  text-white text-center px-3 py-2">
+              Request An Audit
+            </div>
+          </button>
+        </div>
       </div>
     );
 }
